547d81e10c50fc025346636d5e31c9f9111faa14
AbdAlrhman Mohamed <abdalrhnanmohamed1912005@gmail.com>
2026-04-20 23:35:49 +0200

    Refactor environment variables and update base URL handling; remove commented code and unused imports

--- src/utils/baseUrlHandler.js
+++ src/utils/baseUrlHandler.js
@@ -1,6 +1,14 @@
 // Get Current Environment And Set BaseUrl
-export function baseUrlHandler() {
-  return JSON.parse(import.meta.env.VITE_PRODUCTION_ENV)
+// export function baseUrlHandler() {
+//   return JSON.parse(import.meta.env.VITE_PRODUCTION_ENV)
+//     ? import.meta.env.VITE_BACKEND_BASE
+//     : import.meta.env.VITE_BACKEND_BASE_LOCAL;
+// }
+
+
+export const baseUrlHandler = () =>
+  import.meta.env.VITE_PRODUCTION_ENV === "true"
     ? import.meta.env.VITE_BACKEND_BASE
     : import.meta.env.VITE_BACKEND_BASE_LOCAL;
-}
+
+
